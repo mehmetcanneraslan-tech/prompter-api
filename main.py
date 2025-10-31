3d640dd3e9bcc989a2e622e57e54feec6f96d77a
mehmetcanneraslan-tech <mehmetcanneraslan@gmail.com>
2025-10-31 23:24:49 +0300

    fix2

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ app = FastAPI(title="Prompter Backend", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  # Bolt.new veya Vite
-        "http://localhost:3000",  # React
-        "https://yourfrontend.vercel.app",  # deploy edince
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
